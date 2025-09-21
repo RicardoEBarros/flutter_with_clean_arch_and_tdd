ac52fd5c57aa0d7eab98e4ca61eaea922cfbaaee
RicardoEBarros <169519@upf.br>
2025-09-21 14:25:51 -0300

    test: ensure HttpClient request with correct url

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -1,5 +1,7 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:faker/faker.dart';
 import 'package:http/http.dart';
+
+import 'package:flutter_test/flutter_test.dart';
 
 import 'client_spy.dart';
 
@@ -8,25 +10,37 @@ class HttpClient {
 
   HttpClient({required this.client});
 
-  Future<void> get() async {
-    await client.get(Uri());
+  Future<void> get({required String url}) async {
+    await client.get(Uri.parse(url));
   }
 }
 
 void main() {
+  late Faker faker;
+  late String url;
   late ClientSpy client;
   late HttpClient sut;
 
+  setUpAll(() {
+    faker = Faker();
+  });
+
   setUp(() {
     client = ClientSpy();
+    url = faker.internet.httpUrl();
     sut = HttpClient(client: client);
   });
 
   group('get', () {
     test('should request with correct method', () async {
-      await sut.get();
+      await sut.get(url: url);
       expect(client.method, 'get');
       expect(client.callsCount, 1);
+    });
+
+    test('should request with correct url', () async {
+      await sut.get(url: url);
+      expect(client.url, url);
     });
   });
 }
