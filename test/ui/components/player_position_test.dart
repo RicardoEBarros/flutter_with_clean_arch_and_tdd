c840068a5775488ba9499987fd1a78d7c83463cd
RicardoEBarros <169519@upf.br>
2025-09-23 15:16:20 -0300

    test: handle positionless

--- test/ui/components/player_position_test.dart
+++ test/ui/components/player_position_test.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 final class PlayerPosition extends StatelessWidget {
-  final String position;
-  const PlayerPosition({required this.position, super.key});
+  final String? position;
+  const PlayerPosition({this.position, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Goleiro');
+    return Text(_buildPositionLabel());
   }
+
+  String _buildPositionLabel() => switch (position) {
+    'goalkeeper' => 'Goleiro',
+    _ => 'Gândula',
+  };
 }
 
 void main() {
@@ -16,5 +21,11 @@ void main() {
     final sut = MaterialApp(home: PlayerPosition(position: 'goalkeeper'));
     await tester.pumpWidget(sut);
     expect(find.text('Goleiro'), findsOneWidget);
+  });
+
+  testWidgets('should handle positionless', (tester) async {
+    final sut = MaterialApp(home: PlayerPosition(position: null));
+    await tester.pumpWidget(sut);
+    expect(find.text('Gândula'), findsOneWidget);
   });
 }
