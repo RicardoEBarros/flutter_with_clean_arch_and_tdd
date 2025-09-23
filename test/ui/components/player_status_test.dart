57f5679b27ef8d74aaea06ac1ad394bb2e44bd69
RicardoEBarros <169519@upf.br>
2025-09-23 17:07:08 -0300

    test: should present green color when confirmation is true

---
+++ test/ui/components/player_status_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+final class PlayerStatus extends StatelessWidget {
+  final bool isConfirmed;
+  const PlayerStatus({required this.isConfirmed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 16,
+      height: 16,
+      decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.teal),
+    );
+  }
+}
+
+void main() {
+  testWidgets('', (tester) async {
+    const sut = MaterialApp(home: PlayerStatus(isConfirmed: true));
+    await tester.pumpWidget(sut);
+    final decoration = tester.firstWidget<Container>(find.byType(Container)).decoration as BoxDecoration;
+    expect(decoration.color, Colors.teal);
+  });
+}
