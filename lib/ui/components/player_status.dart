f094cc901fdfdd03e9556e45ad952a24735a84cd
RicardoEBarros <169519@upf.br>
2025-09-23 17:16:29 -0300

    refactor: move PlayerStatus to its own file

---
+++ lib/ui/components/player_status.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+final class PlayerStatus extends StatelessWidget {
+  final bool? isConfirmed;
+  const PlayerStatus({this.isConfirmed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 16,
+      height: 16,
+      decoration: BoxDecoration(shape: BoxShape.circle, color: getColor()),
+    );
+  }
+
+  Color getColor() => switch (isConfirmed) {
+    true => Colors.teal,
+    false => Colors.pink,
+    null => Colors.blueGrey,
+  };
+}
