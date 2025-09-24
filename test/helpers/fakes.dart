8b7ac39bbaf3ae4a42682c5090320e33a5f6edd1
RicardoEBarros <169519@upf.br>
2025-09-24 17:22:02 -0300

    test: ensure NextEventRxPresenter map NextEventPlayer to doubt player

--- test/helpers/fakes.dart
+++ test/helpers/fakes.dart
@@ -1,3 +1,5 @@
 import 'package:faker/faker.dart';
 
 String anyString() => faker.randomGenerator.integer(5000).toString();
+bool anyBool() => faker.randomGenerator.boolean();
+DateTime anyDate() => faker.date.dateTime();
