06be8447805d9f837e1c3a71b5848e82bcace74a
RicardoEBarros <169519@upf.br>
2025-09-24 11:36:54 -0300

    test: remove reload method

--- test/ui/pages/next_event_page_test.dart
+++ test/ui/pages/next_event_page_test.dart
@@ -11,8 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../helpers/fakes.dart';
 
 final class NextEventPresenterSpy implements NextEventPresenter {
-  int loadCallsCount = 0;
-  int reloadCallsCount = 0;
+  int callsCount = 0;
   String? groupId;
   bool? isReload;
   var nextEventSubject = BehaviorSubject<NextEventViewModel>();
@@ -49,15 +48,9 @@ final class NextEventPresenterSpy implements NextEventPresenter {
 
   @override
   void loadNextEvent({required String groupId, bool isReload = false}) {
-    loadCallsCount++;
+    callsCount++;
     this.groupId = groupId;
     this.isReload = isReload;
-  }
-
-  @override
-  void reloadNextEvent({required String groupId}) {
-    this.groupId = groupId;
-    reloadCallsCount++;
   }
 }
 
@@ -76,7 +69,7 @@ void main() {
 
   testWidgets('should load event data on page init', (tester) async {
     await tester.pumpWidget(sut);
-    expect(presenter.loadCallsCount, 1);
+    expect(presenter.callsCount, 1);
     expect(presenter.groupId, groupId);
     expect(presenter.isReload, false);
   });
@@ -215,13 +208,13 @@ void main() {
 
   testWidgets('should load event data on reload click', (tester) async {
     await tester.pumpWidget(sut);
-    expect(presenter.loadCallsCount, 1);
+    expect(presenter.callsCount, 1);
     expect(presenter.groupId, groupId);
     expect(presenter.isReload, false);
     presenter.emitError();
     await tester.pump();
     await tester.tap(find.text('Recarregar'));
-    expect(presenter.loadCallsCount, 2);
+    expect(presenter.callsCount, 2);
     expect(presenter.groupId, groupId);
     expect(presenter.isReload, true);
   });
@@ -240,14 +233,14 @@ void main() {
 
   testWidgets('should load event data on pull to refresh', (tester) async {
     await tester.pumpWidget(sut);
-    expect(presenter.loadCallsCount, 1);
+    expect(presenter.callsCount, 1);
     expect(presenter.groupId, groupId);
     expect(presenter.isReload, false);
     presenter.emitNextEvent();
     await tester.pump();
     await tester.flingFrom(const Offset(50, 100), const Offset(0, 400), 800);
     await tester.pumpAndSettle();
-    expect(presenter.loadCallsCount, 2);
+    expect(presenter.callsCount, 2);
     expect(presenter.groupId, groupId);
     expect(presenter.isReload, true);
   });
