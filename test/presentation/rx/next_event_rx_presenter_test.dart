373ea342067ba29b14dd69d2a7635e8ee5ffd077
RicardoEBarros <169519@upf.br>
2025-09-25 07:35:11 -0300

    test: build out list correctly

--- test/presentation/rx/next_event_rx_presenter_test.dart
+++ test/presentation/rx/next_event_rx_presenter_test.dart
@@ -38,6 +38,11 @@ final class NextEventRxPresenter {
         .sortedBy((player) => player.name)
         .map(_mapPlayer)
         .toList(),
+    out: event.players
+        .where((player) => player.confirmationDate != null && !player.isConfirmed)
+        .sortedBy((player) => player.confirmationDate!)
+        .map(_mapPlayer)
+        .toList(),
   );
 
   NextEventPlayerViewModel _mapPlayer(NextEventPlayer player) => NextEventPlayerViewModel(
@@ -113,12 +118,7 @@ void main() {
     nextEventLoader.simulatePlayers([
       NextEventPlayer(id: anyString(), name: 'C', isConfirmed: anyBool()),
       NextEventPlayer(id: anyString(), name: 'A', isConfirmed: anyBool()),
-      NextEventPlayer(
-        id: anyString(),
-        name: 'B',
-        isConfirmed: anyBool(),
-        confirmationDate: anyDate(),
-      ),
+      NextEventPlayer(id: anyString(), name: 'B', isConfirmed: anyBool(), confirmationDate: anyDate()),
       NextEventPlayer(id: anyString(), name: 'D', isConfirmed: anyBool()),
     ]);
     sut.nextEventStream.listen((event) {
@@ -146,6 +146,23 @@ void main() {
       expect(event.doubt[0].isConfirmed, null);
       expect(event.doubt[0].photo, player.photo);
       expect(event.doubt[0].position, player.position);
+    });
+    await sut.loadNextEvent(groupId: groupId);
+  });
+
+  test('should build out list sorted by confirmation date', () async {
+    nextEventLoader.simulatePlayers([
+      NextEventPlayer(id: anyString(), name: 'C', isConfirmed: false, confirmationDate: DateTime(2024, 1, 1, 10)),
+      NextEventPlayer(id: anyString(), name: 'A', isConfirmed: anyBool()),
+      NextEventPlayer(id: anyString(), name: 'B', isConfirmed: true, confirmationDate: DateTime(2024, 1, 1, 11)),
+      NextEventPlayer(id: anyString(), name: 'E', isConfirmed: false, confirmationDate: DateTime(2024, 1, 1, 9)),
+      NextEventPlayer(id: anyString(), name: 'D', isConfirmed: false, confirmationDate: DateTime(2024, 1, 1, 12)),
+    ]);
+    sut.nextEventStream.listen((event) {
+      expect(event.out.length, 3);
+      expect(event.out[0].name, 'E');
+      expect(event.out[1].name, 'C');
+      expect(event.out[2].name, 'D');
     });
     await sut.loadNextEvent(groupId: groupId);
   });
