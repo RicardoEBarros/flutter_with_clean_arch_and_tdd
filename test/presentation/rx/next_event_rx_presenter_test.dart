874cdf56bdc5b2746234ad09a09d4bff5cfe5ed9
RicardoEBarros <169519@upf.br>
2025-09-25 07:59:29 -0300

    feat: make NextEventRxPresenter implements NextEventPresenter

--- test/presentation/rx/next_event_rx_presenter_test.dart
+++ test/presentation/rx/next_event_rx_presenter_test.dart
@@ -10,16 +10,20 @@ import 'package:rxdart/rxdart.dart';
 
 import '../../helpers/fakes.dart';
 
-final class NextEventRxPresenter {
+final class NextEventRxPresenter implements NextEventPresenter {
   final Future<NextEvent> Function({required String groupId}) nextEventLoader;
   final nextEventSubject = BehaviorSubject<NextEventViewModel>();
   final isBusySubject = BehaviorSubject<bool>();
 
   NextEventRxPresenter({required this.nextEventLoader});
 
+  @override
   Stream<bool> get isBusyStream => isBusySubject.stream;
+
+  @override
   Stream<NextEventViewModel> get nextEventStream => nextEventSubject.stream;
 
+  @override
   Future<void> loadNextEvent({required String groupId, bool isReload = false}) async {
     try {
       if (isReload) isBusySubject.add(true);
