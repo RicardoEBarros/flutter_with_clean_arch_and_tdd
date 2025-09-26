19cb2cdbd46a76cf2bad43762e37f148ec75e160
RicardoEBarros <169519@upf.br>
2025-09-26 09:29:55 -0300

    refactor: load events data from file

--- test/e2e/next_event_page_test.dart
+++ test/e2e/next_event_page_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:advanced_flutter/domain/usecases/next_event_loader.dart';
 import 'package:advanced_flutter/infra/api/adapters/http_adapter.dart';
 import 'package:advanced_flutter/infra/api/repositories/load_next_event_api_repo.dart';
@@ -10,66 +12,16 @@ import '../infra/api/mocks/client_spy.dart';
 import '../mocks/fakes.dart';
 
 void main() {
+  late String events;
+
+  setUpAll(() async {
+    final jsonFile = File('test/mocks/event.json');
+    events = await jsonFile.readAsString();
+  });
+
   testWidgets('should present next event page', (tester) async {
     final client = ClientSpy();
-    client.responseJson = '''
-      {
-        "id": "1",
-        "groupName": "Pelada Chega+",
-        "date": "2024-01-11T11:10:00.000Z",
-        "players": [{
-          "id": "1",
-          "name": "Cristiano Ronaldo",
-          "position": "forward",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T11:07:00.000Z"
-        }, {
-          "id": "2",
-          "name": "Lionel Messi",
-          "position": "midfielder",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T11:08:00.000Z"
-        }, {
-          "id": "3",
-          "name": "Dida",
-          "position": "goalkeeper",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T09:10:00.000Z"
-        }, {
-          "id": "4",
-          "name": "Romario",
-          "position": "forward",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T11:10:00.000Z"
-        }, {
-          "id": "5",
-          "name": "Claudio Gamarra",
-          "position": "defender",
-          "isConfirmed": false,
-          "confirmationDate": "2024-01-10T13:10:00.000Z"
-        }, {
-          "id": "6",
-          "name": "Diego Forlan",
-          "position": "defender",
-          "isConfirmed": false,
-          "confirmationDate": "2024-01-10T14:10:00.000Z"
-        }, {
-          "id": "7",
-          "name": "Zé Ninguém",
-          "isConfirmed": false
-        }, {
-          "id": "8",
-          "name": "Rodrigo Manguinho",
-          "isConfirmed": false
-        }, {
-          "id": "9",
-          "name": "Claudio Taffarel",
-          "position": "goalkeeper",
-          "isConfirmed": true,
-          "confirmationDate": "2024-01-10T09:15:00.000Z"
-        }]
-      }
-    ''';
+    client.responseJson = events;
     final httpClient = HttpAdapter(client: client);
     final repo = LoadNextEventApiRepository(httpClient: httpClient, url: anyString());
     final nextEventLoader = NextEventLoader(repo: repo);
