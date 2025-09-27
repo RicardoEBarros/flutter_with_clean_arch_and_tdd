218d091970bad300263f367025e423ddb4226a68
RicardoEBarros <169519@upf.br>
2025-09-27 13:51:04 -0300

    test: ensure LoadNextEventCacheRepository returns NextEvent on success

---
+++ test/infra/cache/repositories/load_next_event_cache_repo_test.dart
@@ -0,0 +1,111 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:advanced_flutter/domain/entities/next_event.dart';
+import 'package:advanced_flutter/domain/entities/next_event_player.dart';
+
+import '../../../mocks/fakes.dart';
+
+final class CacheGetClientSpy implements CacheGetClient {
+  String? key;
+  int callsCount = 0;
+  dynamic response;
+
+  @override
+  Future<dynamic> get({required String key}) async {
+    this.key = key;
+    callsCount++;
+    return response;
+  }
+}
+
+abstract interface class CacheGetClient {
+  Future<dynamic> get({required String key});
+}
+
+final class LoadNextEventCacheRepository {
+  final CacheGetClient cacheClient;
+  final String key;
+
+  const LoadNextEventCacheRepository({required this.cacheClient, required this.key});
+
+  Future<NextEvent> loadNextEvent({required String groupId}) async {
+    final json = await cacheClient.get(key: '$key:$groupId');
+    return NextEventMapper().toObject(json);
+  }
+}
+
+abstract base class Mapper<Entity> {
+  List<Entity> toList(dynamic arr) => arr.map<Entity>(toObject).toList();
+  Entity toObject(dynamic json);
+}
+
+final class NextEventMapper extends Mapper<NextEvent> {
+  @override
+  NextEvent toObject(dynamic json) {
+    return NextEvent(groupName: json['groupName'], date: json['date'], players: NextEventPlayerMapper().toList(json['players']));
+  }
+}
+
+final class NextEventPlayerMapper extends Mapper<NextEventPlayer> {
+  @override
+  NextEventPlayer toObject(dynamic json) {
+    return NextEventPlayer(
+      id: json['id'],
+      name: json['name'],
+      position: json['position'],
+      photo: json['photo'],
+      confirmationDate: json['confirmationDate'],
+      isConfirmed: json['isConfirmed'],
+    );
+  }
+}
+
+void main() {
+  late String groupId;
+  late String key;
+  late CacheGetClientSpy cacheClient;
+  late LoadNextEventCacheRepository sut;
+
+  setUp(() {
+    groupId = anyString();
+    key = anyString();
+    cacheClient = CacheGetClientSpy();
+    cacheClient.response = {
+      "groupName": "any_group_name",
+      "date": DateTime(2024, 1, 1, 10, 30),
+      "players": [
+        {"id": "any_id_1", "name": "any_name_1", "isConfirmed": true},
+        {
+          "id": "any_id_2",
+          "name": "any_name_2",
+          "photo": "any_photo_2",
+          "position": "any_position_2",
+          "confirmationDate": DateTime(2024, 1, 1, 12, 30),
+          "isConfirmed": false,
+        },
+      ],
+    };
+    sut = LoadNextEventCacheRepository(cacheClient: cacheClient, key: key);
+  });
+
+  test('should call CacheAdapter with correct input', () async {
+    await sut.loadNextEvent(groupId: groupId);
+    expect(cacheClient.key, '$key:$groupId');
+    expect(cacheClient.callsCount, 1);
+  });
+
+  test('should return NexEvent on success', () async {
+    final event = await sut.loadNextEvent(groupId: groupId);
+    expect(event.groupName, 'any_group_name');
+    expect(event.date, DateTime(2024, 1, 1, 10, 30));
+    expect(event.players[0].id, 'any_id_1');
+    expect(event.players[0].name, 'any_name_1');
+    expect(event.players[0].isConfirmed, true);
+    expect(event.players[1].id, 'any_id_2');
+    expect(event.players[1].name, 'any_name_2');
+    expect(event.players[1].position, 'any_position_2');
+    expect(event.players[1].photo, 'any_photo_2');
+    expect(event.players[1].confirmationDate, DateTime(2024, 1, 1, 12, 30));
+    expect(event.players[1].isConfirmed, false);
+  });
+}
