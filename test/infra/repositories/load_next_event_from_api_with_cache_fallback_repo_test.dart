63f1cfcc984bb39f35755bb0878956f1adca430d
RicardoEBarros <169519@upf.br>
2025-10-02 10:55:37 -0300

    test: ensure LoadNextEventFromApiWithCacheFallbackRepository return cache data when api fails

--- test/infra/repositories/load_next_event_from_api_with_cache_fallback_repo_test.dart
+++ test/infra/repositories/load_next_event_from_api_with_cache_fallback_repo_test.dart
@@ -26,8 +26,7 @@ final class LoadNextEventFromApiWithCacheFallbackRepository {
       await cacheClient.save(key: '$key:$groupId', value: json);
       return event;
     } catch (e) {
-      await loadNextEventFromCache(groupId: groupId);
-      return NextEvent(groupName: anyString(), date: anyDate(), players: []);
+      return await loadNextEventFromCache(groupId: groupId);
     }
   }
 }
@@ -141,5 +140,11 @@ void main() {
     await sut.loadNextEvent(groupId: groupId);
     expect(cacheRepo.groupId, groupId);
     expect(cacheRepo.callsCount, 1);
+  });
+
+  test('should return cache data when api fails', () async {
+    apiRepo.error = Error();
+    final event = await sut.loadNextEvent(groupId: groupId);
+    expect(event, cacheRepo.output);
   });
 }
