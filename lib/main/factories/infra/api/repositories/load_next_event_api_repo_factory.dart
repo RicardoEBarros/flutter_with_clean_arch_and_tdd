48a266ce5d196fb8c02885dcaa32c6afab761dde
RicardoEBarros <169519@upf.br>
2025-10-08 12:25:25 -0300

    refactor: move mappers creation to factories

--- lib/main/factories/infra/api/repositories/load_next_event_api_repo_factory.dart
+++ lib/main/factories/infra/api/repositories/load_next_event_api_repo_factory.dart
@@ -1,14 +1,9 @@
 import 'package:advanced_flutter/domain/repositories/load_next_event_repository.dart';
 import 'package:advanced_flutter/infra/api/repositories/load_next_event_api_repo.dart';
-import 'package:advanced_flutter/infra/mappers/next_event_mapper.dart';
-import 'package:advanced_flutter/infra/mappers/next_event_player_mapper.dart';
 import 'package:advanced_flutter/main/factories/infra/api/adapters/http_adapter_factory.dart';
+import 'package:advanced_flutter/main/factories/infra/mappers/next_event_mapper_factory.dart';
 
 LoadNextEventRepository makeLoadNextEventApiRepository() {
   const url = 'http://192.168.18.5:8080/api/groups/:groupId/next_event';
-  return LoadNextEventApiRepository(
-    httpClient: makeHttpAdapter(),
-    url: url,
-    mapper: NextEventMapper(playerMapper: NextEventPlayerMapper()),
-  );
+  return LoadNextEventApiRepository(httpClient: makeHttpAdapter(), url: url, mapper: makeNextEventMapper());
 }
--- test/e2e/next_event_page_test.dart
+++ test/e2e/next_event_page_test.dart
@@ -1,13 +1,12 @@
 import 'dart:io';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 
 import 'package:advanced_flutter/infra/api/adapters/http_adapter.dart';
 import 'package:advanced_flutter/infra/api/repositories/load_next_event_api_repo.dart';
-import 'package:advanced_flutter/infra/mappers/next_event_mapper.dart';
-import 'package:advanced_flutter/infra/mappers/next_event_player_mapper.dart';
+import 'package:advanced_flutter/main/factories/infra/mappers/next_event_mapper_factory.dart';
 import 'package:advanced_flutter/presentation/rx/next_event_rx_presenter.dart';
 import 'package:advanced_flutter/ui/pages/next_event_page.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 import '../infra/api/mocks/client_spy.dart';
 import '../mocks/fakes.dart';
@@ -24,11 +23,7 @@ void main() {
     final client = ClientSpy();
     client.responseJson = events;
     final httpClient = HttpAdapter(client: client);
-    final repo = LoadNextEventApiRepository(
-      httpClient: httpClient,
-      url: anyString(),
-      mapper: NextEventMapper(playerMapper: NextEventPlayerMapper()),
-    );
+    final repo = LoadNextEventApiRepository(httpClient: httpClient, url: anyString(), mapper: makeNextEventMapper());
     final presenter = NextEventRxPresenter(nextEventLoader: repo.loadNextEvent);
     final sut = MaterialApp(
       home: NextEventPage(presenter: presenter, groupId: anyString()),
